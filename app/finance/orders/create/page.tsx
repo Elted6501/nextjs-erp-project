16537a6da52ef5aed28bc215ed9373c8fee52b95
Mariann Valdez Venegas <mavave3942@gmail.com>
2025-06-10 14:51:38 -0600

    ADD: Agrega diseño a order y create

---
+++ app/finance/orders/create/page.tsx
@@ -0,0 +1,9 @@
+export default function CreateOrderPage() {
+  return (
+    <main className="flex flex-col gap-4 p-4">
+      <h1 className="text-2xl font-bold">Create New Order</h1>
+      {/* Form for creating a new order will go here */}
+      <p>Form to create a new order will be implemented here.</p>
+    </main>
+  );
+}
