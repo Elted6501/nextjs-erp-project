093109356e9dc11cc3b6d46b18473b25ba9be57e
Mariann Valdez Venegas <mavave3942@gmail.com>
2025-06-27 14:38:29 -0600

    ADD:  Date filter in pending to pay and the components dateinput and dateinputbutton

--- app/finance/pending-to-pay/page.tsx
+++ app/finance/pending-to-pay/page.tsx
@@ -3,10 +3,17 @@ import Button from "@/components/Button";
 import Dropdown from "@/components/Dropdown";
 import DynamicTable from "@/components/DynamicTable";
 import { useEffect, useState } from "react";
-import { FaFileExcel, FaEye, FaTimes, FaRegCheckCircle, FaPlus } from "react-icons/fa";
+import {
+  FaFileExcel,
+  FaEye,
+  FaTimes,
+  FaRegCheckCircle,
+  /*FaPlus,*/
+} from "react-icons/fa";
 import styles from "@/app/finance/page.module.css";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import toExcel from "@/lib/xlsx/toExcel";
+import DateInput from "@/components/DateInput";
 
 type PendingToPayData = {
   id: string;
@@ -33,11 +40,12 @@ const TABLE_COLUMNS = [
 ];
 
 export default function PendingToPayPage() {
-  const router = useRouter();
+  // const router = useRouter();
   const [pendingToPay, setPendingToPay] = useState<PendingToPayData[]>([]);
   const [filteredData, setFilteredData] = useState<PendingToPayData[]>([]);
   const [status, setStatus] = useState("");
   const [description, setDescription] = useState("");
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
   useEffect(() => {
     fetch("/api/finance/pending_to_pay")
@@ -61,13 +69,15 @@ export default function PendingToPayPage() {
 
   const handleExportExcel = async () => {
     const sheetName = "Pending to pay";
-    const content = pendingToPay.map(({ OrderId, Product, Status, DueDate, PayDate }) => ({
-      OrderId,
-      Product,
-      Status,
-      DueDate,
-      PayDate,
-    }));
+    const content = pendingToPay.map(
+      ({ OrderId, Product, Status, DueDate, PayDate }) => ({
+        OrderId,
+        Product,
+        Status,
+        DueDate,
+        PayDate,
+      })
+    );
 
     const columns = [
       { label: "Order Id", value: "OrderId" },
@@ -90,10 +100,14 @@ export default function PendingToPayPage() {
     let data = pendingToPay;
 
     if (status) {
-      data = data.filter((item) => item.Status.toLowerCase().includes(status.toLowerCase()));
+      data = data.filter((item) =>
+        item.Status.toLowerCase().includes(status.toLowerCase())
+      );
     }
     if (description) {
-      data = data.filter((item) => item.OrderId.toString().includes(description.toString()));
+      data = data.filter((item) =>
+        item.OrderId.toString().includes(description.toString())
+      );
     }
 
     setFilteredData(data);
@@ -107,25 +121,50 @@ export default function PendingToPayPage() {
   return (
     <main className={styles.div_principal}>
       <form>
-        <div className={`${styles.div_principal_top} flex gap-4 mb-6 items-center`}>
+        <div
+          className={`${styles.div_principal_top} flex gap-4 mb-6 items-center`}
+        >
           <div className={`${styles.div_busqueda} flex mb-6 items-center`}>
-            <label className="text-[#8b0f14] font-bold" style={{ alignSelf: "flex-start" }}>
-              Order ID:
+            <label
+              className="text-2xl text-[#8b0f14] font-bold"
+              style={{ alignSelf: "flex-start" }}
+            >
+              Filters
             </label>
             <div className={styles.div_hijo_busqueda}>
-              <input
+              {/* <input
                 type="text"
                 placeholder="Search by order id"
                 className="px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 bg-white border-[#a01217] focus:ring-[#a01217] text-black"
                 value={description}
                 onFocus={() => setStatus("")}
                 onChange={(e) => setDescription(e.target.value)}
+              /> */}
+              <Dropdown
+                options={ORDER_STATUS_OPTIONS}
+                onSelect={handleStatusSelect}
+                placeholder={status === "" ? "Select status" : status}
               />
-              <Dropdown options={ORDER_STATUS_OPTIONS} onSelect={handleStatusSelect} placeholder={status === "" ? "Select status" : status} />
+              <Dropdown
+                options={ORDER_STATUS_OPTIONS}
+                onSelect={handleStatusSelect}
+                placeholder={status === "" ? "Select status" : status}
+              />
+              <Dropdown
+                options={ORDER_STATUS_OPTIONS}
+                onSelect={handleStatusSelect}
+                placeholder={status === "" ? "Select status" : status}
+              />
+
+              <DateInput
+              label="Select Date:"
+                selectedDate={selectedDate}
+                onChange={setSelectedDate}
+              />
             </div>
           </div>
           <div className="flex items-end gap-2 flex-row">
-            <Button
+            {/* <Button
               label={
                 <div className="flex items-center gap-2">
                   <span>New Pending to pay</span>
@@ -134,7 +173,7 @@ export default function PendingToPayPage() {
               }
               className="bg-[#a01217] text-white hover:bg-[#8b0f14] transition-colors p-2 w-fit h-fit"
               onClick={() => router.push("/finance/pending-to-pay/create")}
-            />
+            /> */}
             <Button
               label={
                 <div className="flex items-center gap-2">
