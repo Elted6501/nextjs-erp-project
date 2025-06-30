e04042d5d68786fb6961d51c8bba6a0fdbe2b2d5
Mariann Valdez Venegas <mavave3942@gmail.com>
2025-06-30 08:34:44 -0600

    UPDATE: Visual change in pending to pay

--- app/finance/pending-to-pay/page.tsx
+++ app/finance/pending-to-pay/page.tsx
@@ -144,17 +144,21 @@ export default function PendingToPayPage() {
                 options={ORDER_STATUS_OPTIONS}
                 onSelect={handleStatusSelect}
                 placeholder={status === "" ? "Select status" : status}
+                className="mt-1"
               />
               <Dropdown
                 options={ORDER_STATUS_OPTIONS}
                 onSelect={handleStatusSelect}
                 placeholder={status === "" ? "Select status" : status}
+                className="mt-1"
               />
               <Dropdown
                 options={ORDER_STATUS_OPTIONS}
                 onSelect={handleStatusSelect}
                 placeholder={status === "" ? "Select status" : status}
-              />
+                className="mt-1"
+              />  
+              
 
               <DateInput
               label="Select Date:"
